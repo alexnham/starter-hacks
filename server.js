33614b11313d7f64516b5f9847f5e17b09a76712
Lucas L <lucasliao0403@gmail.com>
2024-07-27 16:50:40 -0400

    added endpoints

--- server.js
+++ server.js
@@ -6,15 +6,59 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 dotenv.config()
 
+var cron = require('node-cron');
 
-app.get('/yup', async (req, res) => {
+
+// daily update 
+cron.schedule('0 0 * * *', () => {
+    // TODO: update daily community task
+});
+
+
+app.get('/leaderboard', async (req, res) => {
     try {
-        res.send("yup")
+        res.send("leaderboard")
+
     } catch (e) {
         console.log(e);
         res.sendStatus(500);
     }
 });
+
+app.get('/user/get', async (req, res) => {
+    try {
+        res.send("user")
+
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/getUserTasks', async (req, res) => {
+    try {
+        res.send("user tasks")
+
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/getCommunityTask', async (req, res) => {
+    try {
+        res.send("community task")
+
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+
+
+
+
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
         app.listen(port, () => {
