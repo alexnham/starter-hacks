c91e2138f1e1e0c338637d57e26b2860450b0cc8
AshtonF04 <ashtonfranklin772@gmail.com>
2024-07-27 19:45:31 -0400

    Setup react client with tailwindcss

--- client/App.js
+++ client/App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw`flex justify-center items-center h-full`}> 
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
