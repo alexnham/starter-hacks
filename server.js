345e6889b9b6f7a5a6799492690ce23f53461e90
alex nham <alexnham11@gmail.com>
2024-07-27 15:54:19 -0400

    mongodb serverjs

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const express = require('express');
 const cors = require('cors'); // Require CORS package
 const app = express();
 const port = 3000;
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+dotenv.config()
+
 
 app.get('/yup', async (req, res) => {
     try {
-     res.send("yup")
-    } catch(e) {
-      console.log(e);
-      res.sendStatus(500);
+        res.send("yup")
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
     }
 });
-
-app.listen(port, () => {
-console.log(`Server listening at http://localhost:${port}`);
-});
-
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server listening at http://localhost:${port}`);
+        });
+    })
