4c708e8e755035d9f9ceb1b980b650592a82ada8
Adam <wangadam019@gmail.com>
2024-07-27 20:43:41 -0400

    update community task
    every min

--- server/controllers/communityTaskController.js
+++ server/controllers/communityTaskController.js
@@ -1,4 +1,5 @@
 const CommunityTask = require('../models/CommunityTask');
+const CommunityTaskID = require('../models/CommunityTaskID'); // Correctly import the model
 
 const addCommunityTask = async (req, res) => {
     console.log(req.body);
@@ -38,4 +39,22 @@ const getAllCommunityTasks = async (req, res) => {
     }
 };
 
-module.exports = { addCommunityTask, getCommunityTask, getAllCommunityTasks };
+const updateCommunityTaskID = async () => {
+    try {
+        // Generate a random number between 1 and 10
+        const randomID = Math.floor(Math.random() * 10) + 1;
+
+        // Update the CommunityTaskID document, create if it doesn't exist
+        const updatedTaskID = await CommunityTaskID.findOneAndUpdate(
+            {},
+            { index: randomID }, // Assuming the field to update is named 'id'
+            { new: true, upsert: true } // Create the document if it doesn't exist
+        );
+
+        console.log('CommunityTaskID updated:', updatedTaskID);
+    } catch (error) {
+        console.error('Error updating CommunityTaskID:', error);
+    }
+};
+
+module.exports = { addCommunityTask, getCommunityTask, getAllCommunityTasks, updateCommunityTaskID };
