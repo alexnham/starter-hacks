b0a39b43ec0020f84c6014e0c83954bc46400e71
Adam <wangadam019@gmail.com>
2024-07-27 20:04:09 -0400

    get all communitytasks

--- server/server.js
+++ server/server.js
@@ -1,64 +1,70 @@
 const express = require('express');
 const cors = require('cors'); // Require CORS package
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cron = require('node-cron');
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-dotenv.config()
+const port = process.env.PORT || 3000;
 
-var cron = require('node-cron');
-
-// which community task is the active one?
-// why do we have addTask if we have a task bank in the backend
-const {addCommunityTask, getCommunityTask} = require('./controllers/communityTaskController');
-const { addDailyTask } = require('./controllers/dailyTaskController');
-const { signup, login } = require('./controllers/userController');
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-// daily update 
+// Import controllers
+const { addCommunityTask, getCommunityTask, getAllCommunityTasks } = require('./controllers/communityTaskController');
+const { addDailyTask } = require('./controllers/dailyTaskController');
+const { signup, login } = require('./controllers/userController');
+
+// Daily update cron job
 cron.schedule('* * * * *', () => {
     // TODO: update daily community task
+    console.log('Running a task every minute');
 });
 
-
+// Routes
 app.get('/leaderboard', async (req, res) => {
     try {
-        res.send("leaderboard")
-
+        res.send("leaderboard");
     } catch (e) {
-        console.log(e);
+        console.error(e);
         res.sendStatus(500);
     }
 });
 
 app.get('/user/get', async (req, res) => {
     try {
-        res.send("user")
-
+        res.send("user");
     } catch (e) {
-        console.log(e);
+        console.error(e);
         res.sendStatus(500);
     }
 });
 
 app.get('/getUserTasks', async (req, res) => {
     try {
-        res.send("user tasks")
-
+        res.send("user tasks");
     } catch (e) {
-        console.log(e);
+        console.error(e);
         res.sendStatus(500);
     }
 });
 
+app.get('/tasks/getAllCommunityTasks', getAllCommunityTasks); // New route for getting all community tasks
 app.post('/tasks/addCommunityTask', addCommunityTask);
-app.post('/tasks/addDailyTask', addDailyTask)
-app.post('/user/signup',signup)
-app.post('/user/login',login)
+app.post('/tasks/addDailyTask', addDailyTask);
+app.post('/user/signup', signup);
+app.post('/user/login', login);
 
-mongoose.connect(process.env.MONG_URI)
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONG_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(port, () => {
             console.log(`Server listening at http://localhost:${port}`);
         });
     })
+    .catch(error => {
+        console.error('Database connection error:', error);
+    });
