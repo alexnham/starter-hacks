6b87e8205323e1892b238a409096d9b96874a16b
AshtonF04 <ashtonfranklin772@gmail.com>
2024-07-27 23:13:16 -0400

    Started login frontend

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,22 +1,30 @@
 import { Text, View, SafeAreaView, TextInput } from "react-native";
+import { Link } from "expo-router";
 
 import tw from 'twrnc'
 
 export default function Index() {
   return (
-    <View style={tw`flex h-full`}> 
+    <View style={tw`flex h-full bg-slate-100`}> 
       {/* Header of page displaying username and welcome text */}
       <View style={tw`flex justify-center px-8 py-4 h-32 rounded-3xl bg-green-600 gap-1`}>
         <Text style={tw`text-white font-bold text-3xl`}>Environment App</Text>
       </View>
       {/* Safe area view to hold form */}
       <SafeAreaView style={tw`flex mx-8 my-8 gap-8`}>
-        <View style={tw`flex items-center py-12`}>
+        <View style={tw`flex items-center py-12 gap-5`}>
           <Text style={tw`font-bold text-2xl`}>Login</Text>
-          <TextInput 
-            placeholder="Enter email"
-            style={tw``}
-          />
+          <View style={tw`w-full flex gap-5`}>
+            <TextInput 
+              placeholder="Enter email"
+              style={tw`bg-white w-full h-12 rounded-md text-lg px-4 py-1`}
+            />
+            <TextInput 
+              placeholder="Enter password"
+              style={tw`bg-white w-full h-12 rounded-md text-lg px-4 py-1`}
+            />
+          </View>
+          <Link href='/signup'>Sign Up</Link>
         </View>
       </SafeAreaView>
     </View>
