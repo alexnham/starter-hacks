0056c5252228bab62586f86fd587c26af6916398
alex nham <alexnham11@gmail.com>
2024-07-27 20:32:50 -0400

    returning full user

--- models/CommunityTaskID.js
+++ models/CommunityTaskID.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema
 
 const communityTaskIDSchema = new Schema({
     //community task id
+    index: {
+        type: Number,
+        required: true
+    }
 })
 
 module.exports = mongoose.model('CommunityTaskID', communityTaskIDSchema);
